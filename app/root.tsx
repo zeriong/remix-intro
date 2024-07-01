8d246b0d0ad356afc912944db06816b5898e6857
jeryong <jaeryong95@gmail.com>
2024-07-01 23:44:04 +0900

    feat: remix 기반의 다양한 Math convertor 구성을 위해 Layout 기반 구축

--- app/root.tsx
+++ app/root.tsx
@@ -23,7 +23,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <div className="text-2xl text-blue-500">하하ㅏ하하하하</div>
         {children}
         <ScrollRestoration />
         <Scripts />
