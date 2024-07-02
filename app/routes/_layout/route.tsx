3c1c0beb92bf0591a880493f43410d0000c6b5b6
jeryong <jaeryong95@gmail.com>
2024-07-02 19:33:52 +0900

    design: 헤더 -> 메뉴 구성

--- app/routes/_layout/route.tsx
+++ app/routes/_layout/route.tsx
@@ -3,11 +3,17 @@ import { Link, Outlet } from "@remix-run/react";
 export default function Layout() {
   return (
     <>
-      <header className="border-b border-brand-400 py-6 px-8">
-        <Link to="/" className="text-4xl font-extrabold flex items-center">
+      <header className="flex justify-between items-center border-b border-brand-400 px-8 w-full h-[84px]">
+        <Link
+          to="/"
+          className="text-4xl font-extrabold flex items-center w-fit"
+        >
           <span className="text-brand-500 text-5xl">Z</span>
           -Convertor
         </Link>
+        <button className="border rounded-md px-5 py-3 font-extrabold border-brand-400 bg-brand-500 text-[20px] text-white">
+          Menu
+        </button>
       </header>
       <Outlet />
     </>
