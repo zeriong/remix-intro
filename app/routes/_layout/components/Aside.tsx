2a56b8628210c4108af31aff06f2d0e299223750
jeryong <jaeryong95@gmail.com>
2024-07-04 18:09:46 +0900

    feat: aside 레이아웃 추가

---
+++ app/routes/_layout/components/Aside.tsx
@@ -0,0 +1,7 @@
+export default function Aside() {
+  return (
+    <aside className="fixed right-0 bottom-0 shadow-2xl h-[calc(100%-86px)] bg-brand-500 p-3">
+      <div className="bg-red-500">사이드바</div>
+    </aside>
+  );
+}
--- app/routes/_layout/route.tsx
+++ app/routes/_layout/route.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "@remix-run/react";
-import Header from "~/routes/_layout._index/components/Header";
+import Header from "~/routes/_layout/components/Header";
+import Aside from "~/routes/_layout/components/Aside";
 
 export default function Layout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Aside />
     </>
   );
 }
