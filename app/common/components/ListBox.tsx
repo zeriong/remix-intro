b13651981bd27beab8a1dd589e6edf95995e330a
jeryong <jaeryong95@gmail.com>
2024-07-08 09:30:34 +0900

    refactor: ListBox 타이틀 추가 및 기본 스타일 개선

--- app/common/components/ListBox.tsx
+++ app/common/components/ListBox.tsx
@@ -25,13 +25,13 @@ const ListBox = ({ list, setSelectState, selectState }: ICustomListBox) => {
       {/* 리스트박스 버튼 */}
       <ListboxButton
         className={clsx(
-          "relative block w-full rounded-lg bg-brand-500 py-1.5 pr-8 pl-3 text-left text-sm/6 text-white",
+          "relative block w-full rounded-lg bg-gray-50/80 py-1.5 pr-8 pl-3 text-left text-[18px] font-bold text-gray-600 border-2 border-gray-200",
           "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
         )}
       >
         {selectState.name}
         <ChevronDownIcon
-          className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white/60"
+          className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-default-black"
           aria-hidden="true"
         />
       </ListboxButton>
@@ -42,7 +42,7 @@ const ListBox = ({ list, setSelectState, selectState }: ICustomListBox) => {
         anchor="bottom"
         transition
         className={clsx(
-          "w-[var(--button-width)] rounded-xl border border-white/5 bg-brand-400 p-1 [--anchor-gap:var(--spacing-1)] focus:outline-none",
+          "w-[var(--button-width)] rounded-xl border border-white/5 bg-gray-100 p-1 [--anchor-gap:var(--spacing-1)] focus:outline-none",
           "transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0"
         )}
       >
@@ -51,14 +51,14 @@ const ListBox = ({ list, setSelectState, selectState }: ICustomListBox) => {
           <ListboxOption
             key={item.value}
             value={item}
-            className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
+            className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/70"
           >
             <CheckIcon
-              className={`size-4 fill-white ${
+              className={`size-4 fill-default-black ${
                 selectState.value === item.value ? "visible" : "invisible"
               }`}
             />
-            <div className="text-sm/6 text-white">{item.name}</div>
+            <div className="text-[17px] font-bold">{item.name}</div>
           </ListboxOption>
         ))}
       </ListboxOptions>
--- app/routes/_layout._index/route.tsx
+++ app/routes/_layout._index/route.tsx
@@ -10,13 +10,14 @@ export default function Index() {
 
   return (
     <section className="grid place-content-center w-full h-full">
-      <div className="my-auto bg-brand-400 p-4 w-[500px] h-[500px]">
-        리스트박스...
+      {/* content area */}
+      <div className="my-auto p-4 w-[500px] h-[500px] rounded bg-white border-2 border-gray-50 shadow-md">
+        <p className="font-bold text-[24px] mb-3">Total Convertor</p>
         <ListBox
           list={[
-            { value: 1, name: "Room Size" },
-            { value: 2, name: "Length" },
-            { value: 3, name: "Data" },
+            { value: 1, name: "방 크기" },
+            { value: 2, name: "길이" },
+            { value: 3, name: "데이터 크기" },
           ]}
           setSelectState={setCategory}
           selectState={category}
